1b4c772146737baf755893a53b86a5e85c40c985
Brett Knowles <brettknowles@manjaro.attlocal.net>
2021-10-11 21:19:13 -0500

    Add initial files to repo

---
+++ rockPaperScissors.js
@@ -0,0 +1,11 @@
+let userScore = 0;
+let computerScore = 0;
+let roundWinner = '';
+
+const moveArray = ["Rock", "Paper", "Scissors"];
+const randomMove = Math.floor(Math.random() * moveArray.length);
+
+console.log(moveArray[randomMove]);
+
+function computerPlay(moveChoice){
+}
