c604b507ca95c6cecb5debe878992aa98d3be18a
Brett Knowles <bkaudev@yahoo.com>
2021-10-23 16:21:53 -0500

    Updated Javascript file

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,37 +1,34 @@
-//Variable for storage of user selection
-var playerSelection = window.prompt ("Rock, paper, or scissors?");
-
 //Function that stores the computer's move
 function computerPlay(){;
     //Formula that draws random number between integers 1 and 3
     let choiceNumber = Math.floor(Math.random () * 3);
-    //Displays Rock, Paper, or Scissors depending on which integer is chosen at random
+    //Displays rock, paper, or scissors depending on which integer is chosen at random
     if (choiceNumber == 1) {
-        return "Rock";
+        return "rock";
     } else if (choiceNumber == 2) {
-        return "Paper";
-    } else { return "Scissors";
+        return "paper";
+    } else { return "scissors";
     }
        }
 
-//This function takes the computer's selection and player's selection to play a round
-function playRound(playerSelection){;
-    //Sets a variable equal to the computerPlay function to use the choice made in said variable
-    let computerSelection = computerPlay()
+function playRound(playerSelection, computerSelection) {
 
     if (playerSelection == computerSelection){
         return "It's a tie!";
     }
-    else if ((playerSelection = "Rock" && computerSelection == "Scissors") ||
-            (playerSelection = "Paper" && computerSelection == "Rock") ||
-            (playerSelection = "Scissors" && computerSelection == "Paper")){
+    else if ((playerSelection == "rock" && computerSelection == "scissors") ||
+            (playerSelection == "paper" && computerSelection == "rock") ||
+            (playerSelection == "scissors" && computerSelection == "paper")){
         return "You win!"
     }
-    else if ((playerSelection = "Rock" && computerSelection == "Paper") ||
-            (playerSelection = "Paper" && computerSelection == "Scissors") ||
-            (playerSelection = "Scissors" && computerSelection == "Rock")){
+    else if ((playerSelection == "rock" && computerSelection == "paper") ||
+            (playerSelection == "paper" && computerSelection == "scissors") ||
+            (playerSelection == "scissors" && computerSelection == "rock")){
         return "You lose..."
             }
     }
+      
+const playerSelection = "rock".toLowerCase();
+const computerSelection = computerPlay();
 
-console.log(playRound())
+console.log(playRound(playerSelection, computerSelection));
