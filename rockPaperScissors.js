4fc10f89f0d2fce1c57a5c9181a64a1631494cd0
Brett Knowles <bkaudev@yahoo.com>
2021-10-23 18:25:54 -0500

    Added comments. Added shell function skeleton for 'game'. Does not return or console log anything in this form.

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -11,16 +11,20 @@ function computerPlay(){;
     }
        }
 
+//Plays one round of the game. Used to make all comparison's to dictate the victor of a round.
 function playRound(playerSelection, computerSelection) {
 
+    //Makes comparison that creates tie.
     if (playerSelection == computerSelection){
         return "It's a tie!";
     }
+    //Makes comparison's that win for player.
     else if ((playerSelection == "rock" && computerSelection == "scissors") ||
             (playerSelection == "paper" && computerSelection == "rock") ||
             (playerSelection == "scissors" && computerSelection == "paper")){
         return "You win!"
     }
+    //Makes comparison's that lose for player.
     else if ((playerSelection == "rock" && computerSelection == "paper") ||
             (playerSelection == "paper" && computerSelection == "scissors") ||
             (playerSelection == "scissors" && computerSelection == "rock")){
@@ -31,4 +35,7 @@ function playRound(playerSelection, computerSelection) {
 const playerSelection = "rock".toLowerCase();
 const computerSelection = computerPlay();
 
-console.log(playRound(playerSelection, computerSelection));
+function game(){
+
+}
+
