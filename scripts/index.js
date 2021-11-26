3befec7be53364037d23c5ece63d4cccb403e64b
Brett Knowles <bkaudev@yahoo.com>
2021-11-26 13:32:23 -0600

    Start the script file, create working random choice function for computer that console logs answer

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,7 @@
-console.log("Hello");
+function computerPlay(){
+    let choices = ["rock","paper","scissors"];
+    let compChoice = choices[Math.floor(Math.random() * choices.length)];
+    return compChoice;
+}
+
+console.log(computerPlay());
