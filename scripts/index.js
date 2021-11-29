61d8e05648a556005db5558ec35fb9dae43c7fb8
Brett Knowles <bkaudev@yahoo.com>
2021-11-29 10:31:58 -0600

    Move to step 2 of Odin by creating playRound function and two variables. Console log result of playRound.

--- scripts/index.js
+++ scripts/index.js
@@ -4,4 +4,10 @@ function computerPlay(){
     return compChoice;
 }
 
-console.log(computerPlay());
+function playRound(playerSelection, computerSelection){
+    //to be made
+}
+
+const playerSelection = "rock"
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
