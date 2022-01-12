a4bc42855955f5ebe1e2b741f1f2968af0072136
Brett Knowles <bkaudev@yahoo.com>
2022-01-12 12:27:29 -0600

    Console log round winner instead of return in playRound function.

--- scripts/index.js
+++ scripts/index.js
@@ -11,19 +11,19 @@ function playRound(computerSelection){
     let playerSelection = prompt("Rock, paper, or scissors?");
     playerSelection = playerSelection.toLowerCase();
     if (playerSelection == computerSelection){
-         return "Tie!";
+         console.log("Tie!");
         }
     else if ((playerSelection == "rock" && computerSelection == "scissors") ||
             (playerSelection == "paper" && computerSelection == "rock") ||
             (playerSelection == "scissors" && computerSelection == "paper")){
                 ++playerScore;
-                 return "You win this round!"
+                 console.log("You win this round!");
         }
     else if ((playerSelection == "rock" && computerSelection == "paper") ||
             (playerSelection == "paper" && computerSelection == "scissors") ||
             (playerSelection == "scissors" && computerSelection == "rock")){
                 ++computerScore;
-                return "You lose this round..."
+                console.log("You lose this round...");
         }
     }    
 
